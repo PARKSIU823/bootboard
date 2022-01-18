3f9d92181228e1de61d7dd68de597682cf1550fd
PARK SIU <parksiu823@gmail.com>
2022-01-18 16:35:17 +0900

    210118 리스트 출력

--- src/main/java/com/zegra/bootboard/controller/BoardController.java
+++ src/main/java/com/zegra/bootboard/controller/BoardController.java
@@ -1,18 +1,28 @@
 package com.zegra.bootboard.controller;
 
+import com.zegra.bootboard.dto.PageRequestDTO;
+import com.zegra.bootboard.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/bboard")
 @Log4j2
+@RequiredArgsConstructor
 public class BoardController {
 
-    @RequestMapping({"/","/list"})
-    public String list() {
+    private final BoardService boardService;
 
-        log.info("list ============" );
+    @RequestMapping({"/","/list"})
+    public String list(PageRequestDTO pageRequestDTO, Model model) {
+
+        log.info("list ============" + pageRequestDTO);
+
+        model.addAttribute("result", boardService.getList(pageRequestDTO));
 
         return "/board/list";
     }
