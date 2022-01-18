d07114889ff3f7cb929a550df16c3baac0e761ce
PARK SIU <parksiu823@gmail.com>
2022-01-18 16:56:32 +0900

    210118 등록 기능 추가

--- src/main/java/com/zegra/bootboard/controller/BoardController.java
+++ src/main/java/com/zegra/bootboard/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.zegra.bootboard.controller;
 
+import com.zegra.bootboard.dto.BoardDTO;
 import com.zegra.bootboard.dto.PageRequestDTO;
 import com.zegra.bootboard.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/bboard")
@@ -17,6 +21,12 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    /**
+     * 게시글 리스트
+     * @param pageRequestDTO
+     * @param model
+     * @return
+     */
     @RequestMapping({"/","/list"})
     public String list(PageRequestDTO pageRequestDTO, Model model) {
 
@@ -25,6 +35,25 @@ public class BoardController {
         model.addAttribute("result", boardService.getList(pageRequestDTO));
 
         return "/board/list";
+    }
+
+    @GetMapping("/regist")
+    public String regist() {
+        log.info("regist GET ==============");
+
+        return "/board/regist";
+    }
+
+    @PostMapping("/regist")
+    public String registPost(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
+
+        log.info("boardDTO ============" + boardDTO);
+
+        boardDTO.setDelYn('N');
+        Long bno = boardService.register(boardDTO);
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/bboard/list";
     }
 
 }
